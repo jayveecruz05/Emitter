c919fbcb56508900c1cf42fe34343c217b5da389
jayveecruz05 <jayveecruz05@gmail.com>
2018-09-28 15:03:15 +0800

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -3,16 +3,27 @@
 // Emitter
 var emitter = (function () {
     var eventList = {},
-    
+
         onEvent = function(name, method) {
             // console.log(name, method);
             eventList[name] = eventList[name] || [];
             eventList[name].push(method);
         },
 
+        offEvent = function(name) {
+            // console.log(name);
+            if (name) {
+                if (eventList[name]) {
+                    delete eventList[name];
+                }
+            } else {
+                eventList = {};
+            }
+        },
+
         emitEvent = function(name, data) {
             // console.log(name, data);
-            if (eventList[name] != undefined) {
+            if (eventList[name]) {
                 for (var i = 0; i < eventList[name].length; i++) {
                     eventList[name][i](data);
                 }
@@ -21,9 +32,25 @@ var emitter = (function () {
 
     return {
         on: onEvent,
+        off: offEvent,
         emit: emitEvent
     }
 }());
+
+// Setup Tutorial
+// Import Custom Emitter
+// const emitter = require('./emitter');
+
+// Custom Emit Listener
+// emitter.on('greet', function() {
+//     console.log('Hello World!');
+// });
+
+// Custom Emit Off
+// emitter.off('greet');
+
+// Custom Execute Emitter
+// emitter.emit('greet');
 
 // Use module.export to export the .js files
 module.exports = emitter;
